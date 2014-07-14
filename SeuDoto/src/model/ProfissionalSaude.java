947ee6c5b914995e96d34efade2c07351228fd00
Tales Tenorio Pimentel <tales.tsp@gmail.com>
2014-07-14 19:49:49 -0300

    ProfissionalSaude.java versao1 pronta

--- SeuDoto/src/model/ProfissionalSaude.java
+++ SeuDoto/src/model/ProfissionalSaude.java
@@ -9,12 +9,21 @@ public class ProfissionalSaude {
 	private ArrayList<String> especialidades; //trocar por um ENUM?
 	private ArrayList<String> convenios; //trocar por um ENUM?
 	private Endereco endereco;
+	
 	public ProfissionalSaude(TipoProfissional tipo, String numeroRegistro) {
 		this.tipo = tipo;
 		this.numeroRegistro = numeroRegistro;
 		especialidades = new ArrayList<String>();
 		convenios = new ArrayList<String>();
 
+	}
+	
+	public void setTipo(TipoProfissional tipo){
+		this.tipo = tipo;
+	}
+	
+	public void setRegistro(String numeroRegistro){
+		this.numeroRegistro = numeroRegistro;
 	}
 	
 	public void addEspecialidade(String especialidade){
@@ -27,6 +36,26 @@ public class ProfissionalSaude {
 	
 	public void setEndereco(String ruaNumeroBairro, String sala, String cidade, String estado){
 		endereco = new Endereco (ruaNumeroBairro, sala, cidade, estado);
+	}
+
+	public TipoProfissional getTipo() {
+		return tipo;
+	}
+
+	public String getNumeroRegistro() {
+		return numeroRegistro;
+	}
+
+	public ArrayList<String> getEspecialidades() {
+		return especialidades;
+	}
+
+	public ArrayList<String> getConvenios() {
+		return convenios;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
 	}
 
 }
