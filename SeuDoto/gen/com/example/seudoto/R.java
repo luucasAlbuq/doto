12887e2076a4298e0c76c2912d175fa9b2468ab4
luucasAlbuq <lucas.almeida@ccc.ufcg.edu.br>
2014-07-16 01:29:18 -0300

    Activity de buscar

--- SeuDoto/gen/com/example/seudoto/R.java
+++ SeuDoto/gen/com/example/seudoto/R.java
@@ -972,7 +972,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int acess=0x7f020057;
+        public static final int doctor_icon=0x7f020058;
+        public static final int ic_launcher=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -989,6 +991,7 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int botaoLogin=0x7f050045;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1034,6 +1037,15 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int widget43=0x7f050043;
+        public static final int widget44=0x7f050044;
+        public static final int widget52=0x7f05003c;
+        public static final int widget53=0x7f05003d;
+        public static final int widget54=0x7f05003e;
+        public static final int widget55=0x7f05003f;
+        public static final int widget56=0x7f050040;
+        public static final int widget57=0x7f050041;
+        public static final int widget58=0x7f050042;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1085,8 +1097,9 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_busca=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1131,6 +1144,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_busca=0x7f0a000f;
     }
     public static final class style {
         /** 
--- SeuDoto/src/activity/MainActivity.java
+++ SeuDoto/src/activity/MainActivity.java
@@ -3,15 +3,33 @@ package activity;
 import com.example.seudoto.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-
+	
+	private ImageButton botaoLogin;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        botaoLogin = (ImageButton) findViewById(R.id.botaoLogin);
+        
+        botaoLogin.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				 
+				Intent telaBusca = new Intent(MainActivity.this, Busca.class);
+				MainActivity.this.startActivity(telaBusca);
+			}
+		});
     }
 }
