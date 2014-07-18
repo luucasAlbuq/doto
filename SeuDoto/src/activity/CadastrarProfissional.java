cdb8949286679b32b2eb4dfebab0f2235e6f4ef8
luucasAlbuq <lucas.almeida@ccc.ufcg.edu.br>
2014-07-17 23:50:47 -0300

    Tela primitiva detalhes 2

---
+++ SeuDoto/src/activity/CadastrarProfissional.java
@@ -0,0 +1,18 @@
+package activity;
+
+import com.example.seudoto.R;
+import com.example.seudoto.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CadastrarProfissional extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastrar_profissional);
+	}
+}
