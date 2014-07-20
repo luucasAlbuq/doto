ef6d492effaec4fc47d2a174bcf1f8a7b43f1b0c
luucasAlbuq <lucas.almeida@ccc.ufcg.edu.br>
2014-07-19 21:22:26 -0300

    Modificao na Activity e na GUI de cadastrar

--- SeuDoto/src/activity/CadastrarProfissionalActivity.java
+++ SeuDoto/src/activity/CadastrarProfissionalActivity.java
@@ -1,14 +1,25 @@
 package activity;
 
+import util.Convenio;
+import util.Especialidade;
+
 import com.example.seudoto.R;
 import com.example.seudoto.R.layout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CadastrarProfissionalActivity extends Activity {
 
@@ -16,6 +27,23 @@ public class CadastrarProfissionalActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cadastrar_profissional);
+		
+		carregarCidades();
+		carregarConvenios();
+		carregarEspecialidades();
+		carregarTiposProfissionais();
+		
+		ImageView salvar = (ImageView) findViewById(R.id.SaveimageView);
+		
+		 final Toast alertaSucesso = Toast.makeText(this, "Profissional Cadastrado com Sucesso", Toast.LENGTH_LONG);
+		salvar.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				alertaSucesso.show();
+			}
+		});
 	}
 	
 	@Override
@@ -42,4 +70,145 @@ public class CadastrarProfissionalActivity extends Activity {
         Intent i = new Intent(CadastrarProfissionalActivity.this, BuscarActivity.class);
         startActivity(i);
     }
+	
+	public void carregarEspecialidades() {
+		final String[] especialidades = new String[] {
+				Especialidade.CARDIOLOGISTA.toString(),
+				Especialidade.DERMATOLOGISTA.toString(),
+				Especialidade.GINECOLOGISTA.toString(),
+				Especialidade.NEUROLOGISTA.toString(),
+				Especialidade.PEDIATRA.toString() };
+
+		ArrayAdapter<String> adaptadorEspecialidade = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item, especialidades);
+
+		Spinner especialidadeSpinner = (Spinner) findViewById(R.id.cadastro_especialidade_spinner);
+
+		adaptadorEspecialidade
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		especialidadeSpinner.setAdapter(adaptadorEspecialidade);
+
+		especialidadeSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int position, long id) {
+						
+						//TODO
+
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+	}
+	
+	public void carregarConvenios() {
+
+		final String[] convenios = new String[] { Convenio.SMILE.toString(),
+				Convenio.UNIDENTES.toString(), Convenio.UNIMED.toString() };
+
+		ArrayAdapter<String> adaptadorConvenios = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item, convenios);
+
+		Spinner convenioSpinner = (Spinner) findViewById(R.id.cadastro_convenio_spinner);
+
+		adaptadorConvenios
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		convenioSpinner.setAdapter(adaptadorConvenios);
+
+		convenioSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int position, long id) {
+
+						//TODO
+
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+	}
+
+	public void carregarCidades() {
+
+		final String[] cidades = new String[] { "João Pessoa", "Campina Grande",
+				"Patos", "Cajazeiras", "Guarabira", "Sousa" };
+
+		ArrayAdapter<String> adaptadorCidades = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, cidades);
+
+		Spinner cidadesSpinner = (Spinner) findViewById(R.id.cadastro_cidade_spinner);
+
+		adaptadorCidades
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		cidadesSpinner.setAdapter(adaptadorCidades);
+
+		cidadesSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int position, long id) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+	}
+
+	public void carregarTiposProfissionais() {
+
+		final String[] tiposProfissionais = new String[] { "Médico", "Dentista",
+				"Fisioterapeuta", "Nutricionista", "Psicologo" };
+
+		ArrayAdapter<String> adaptadorTiposProfissionais = new ArrayAdapter<String>(
+				this, android.R.layout.simple_spinner_item, tiposProfissionais);
+
+		Spinner profissionaisSpinner = (Spinner) findViewById(R.id.cadastro_tipo_prof_spinner);
+
+		adaptadorTiposProfissionais
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		profissionaisSpinner.setAdapter(adaptadorTiposProfissionais);
+
+		profissionaisSpinner
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+					@Override
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int position, long id) {
+						//TODO
+
+					}
+
+					@Override
+					public void onNothingSelected(AdapterView<?> parent) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+
+	}
 }
