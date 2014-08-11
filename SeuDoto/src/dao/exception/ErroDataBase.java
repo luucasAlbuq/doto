f5067df1b88c037a979aeb2c47f2882befe9a2d3
gillesmedeiros <gilles.medeiros@gmail.com>
2014-08-11 12:50:14 -0300

    11082014 - gillesmedeiros - Exception geral para problema no DAO.

---
+++ SeuDoto/src/dao/exception/ErroDataBase.java
@@ -0,0 +1,13 @@
+package dao.exception;
+
+public class ErroDataBase extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2281718965953405640L;
+
+	public ErroDataBase(String mensagem) {
+		super(mensagem);
+	}
+}
