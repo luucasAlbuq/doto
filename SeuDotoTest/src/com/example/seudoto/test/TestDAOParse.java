54cc23952a4f7d5620b31e7b81c5ace10a7a7af1
talestsp <tales.tsp@gmail.com>
2014-08-16 20:22:07 -0300

    Cadastrar profissional testado ao máximo.

--- SeuDotoTest/src/com/example/seudoto/test/TestDAOParse.java
+++ SeuDotoTest/src/com/example/seudoto/test/TestDAOParse.java
@@ -1,43 +1,39 @@
 package com.example.seudoto.test;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 import model.ProfissionalSaude;
+import android.test.AndroidTestCase;
 
 import com.parse.Parse;
 import com.parse.ParseException;
 
-import controller.ProfissionalController;
 import dao.DAOParse;
 import exception.ProfissionalSaudeException;
-import android.test.AndroidTestCase;
-import com.parse.*;
 
 public class TestDAOParse extends AndroidTestCase {
 	
+	private ArrayList<String> crmProfsFake;
+	
 	protected void setUp() throws Exception {
-		
+		crmProfsFake = new ArrayList<String>();
+		Parse.initialize(getContext(), "7EqoOy2DDnMMHlXUGAbi9aJkqIgryikPIRv7CzX3", "nvT2iYY8AfuMJgH7cJQeoemBFFQsrXDqoR4wy2OJ");
 		super.setUp();
 				
 	}
 	
 	protected void tearDown() throws Exception {
+		removeProfissinaisFakeFromTest();
 		super.tearDown();
 	}
-	
+
 	public void testCadastrarProfissionalComSucesso(){
-		Exception exception = null;
+		Exception exception = new Exception();
 		try {
 			ProfissionalSaude p1 = new ProfissionalSaude("Medico", "abc-124", "TalesBoy", "Psiquiatra", "Particular");
-			ProfissionalSaude p2 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001111", "Nazareno", "Terapeuta Social", "SUS");
-			ProfissionalSaude p3 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001110", "Gesuis Krysto Todo Poderoso Alpha e Omega e etc e tal", "Placebista", "UniCristao");
-			ProfissionalSaude p4 = new ProfissionalSaude("Medico", "1010101", "Timothy Leary", "Experimentalista", "Particular");
-			
 			DAOParse.getInstance().cadastrarProfissional(p1);
-			DAOParse.getInstance().cadastrarProfissional(p2);
-			DAOParse.getInstance().cadastrarProfissional(p3);
-			DAOParse.getInstance().cadastrarProfissional(p4);
-			
+			crmProfsFake.add(p1.getNumeroRegistro());
+			exception = null;
 			
 		} catch (ProfissionalSaudeException e) {
 			e.printStackTrace();
@@ -45,78 +41,206 @@ public class TestDAOParse extends AndroidTestCase {
 			exception = e;
 			e.printStackTrace();
 		}
-		
+		assertNull(exception);
+
+		exception = new Exception();
+		try {
+			ProfissionalSaude p2 = new ProfissionalSaude("Medico", "1010101", "Timothy Leary", "Experimentalista", "Particular");
+			DAOParse.getInstance().cadastrarProfissional(p2);
+			crmProfsFake.add(p2.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			exception = e;
+			e.printStackTrace();
+		}
+
 		assertNull(exception);
 	}
 	
-	public void testCadastrarProfissionalComFalhas(){
-		Exception exception = null;
-		
+	public void testCadastrarProfissionalCRMLongoComSucesso(){
+		Exception exception = new Exception();
 		try {
-			ProfissionalSaude p1 = new ProfissionalSaude("Medico", "abc-124", "TalesBoy", "Psiquiatra", "Particular");		
-			ProfissionalSaude p2 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001111", "Nazareno", "Terapeuta Social", "SUS");
-			ProfissionalSaude p3 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001110", "Gesuis Krysto Todo Poderoso Alpha e Omega e etc e tal", "Placebista", "UniCristao");
-			ProfissionalSaude p4 = new ProfissionalSaude("Medico", "1010101", "Timothy Leary", "Experimentalista", "Particular");
-			DAOParse.getInstance().cadastrarProfissional(p1);
-	
-			
-		} catch (ProfissionalSaudeException e) {
-			e.printStackTrace();
-			
-		}catch (ParseException e) {
-			exception = e;
-			e.printStackTrace();
-		}
-		
-		assertNotNull(exception);
-		exception = null;
-		
-		try {
-			ProfissionalSaude p2 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001111", "Nazareno", "Terapeuta Social", "SUS");
-			DAOParse.getInstance().cadastrarProfissional(p2);
-	
-			
-		} catch (ProfissionalSaudeException e) {
-			e.printStackTrace();
-			
-		}catch (ParseException e) {
-			exception = e;
-			e.printStackTrace();
-		}
-		
-		assertNotNull(exception);
-		exception = null;
-		
-		try {
-			ProfissionalSaude p3 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001110", "Gesuis Krysto Todo Poderoso Alpha e Omega e etc e tal", "Placebista", "UniCristao");
+			ProfissionalSaude p3 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001111", "Nazareno", "Terapeuta Social", "SUS");			
 			DAOParse.getInstance().cadastrarProfissional(p3);
-	
+			crmProfsFake.add(p3.getNumeroRegistro());
+			exception = null;
 			
 		} catch (ProfissionalSaudeException e) {
 			e.printStackTrace();
-			
-		}catch (ParseException e) {
+		} catch (ParseException e) {
 			exception = e;
 			e.printStackTrace();
 		}
+		assertNull(exception);
 		
+		exception = new Exception();
+		try {
+			ProfissionalSaude p4 = new ProfissionalSaude("Medico", "101010100001111000011110000111100001110", "Gesuis Krysto Todo Poderoso Alpha e Omega e etc e tal", "Placebista", "UniCristao");
+			DAOParse.getInstance().cadastrarProfissional(p4);		
+			crmProfsFake.add(p4.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			exception = e;
+			e.printStackTrace();
+		}
+		assertNull(exception);
+	}
+	
+	public void testCadastrarProfissionalCRMRepetido(){
+		boolean uniqueCRM = true;
+		try {
+			ProfissionalSaude p1 = new ProfissionalSaude("Medico", "abc-124", "TalesBoy-CRMRepetido", "Psiquiatra", "Particular");			
+			uniqueCRM = DAOParse.getInstance().isCrmUnico(p1.getNumeroRegistro());
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			fail("Problemas com ProfissionalSaude");
+		} catch (ParseException e) {
+			e.printStackTrace();
+			fail("Problemas com Parse");
+		}
+		assertTrue(uniqueCRM);
+		
+		uniqueCRM = true;
+		try {
+			ProfissionalSaude p2 = new ProfissionalSaude("Medico", "1010101", "Timothy Leary - CRMRepetido", "Experimentalista", "Particular");
+			uniqueCRM = DAOParse.getInstance().isCrmUnico(p2.getNumeroRegistro());
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			fail("Problemas com ProfissionalSaude");
+		} catch (ParseException e) {
+			e.printStackTrace();
+			fail("Problemas com Parse");
+		}
+		assertTrue(uniqueCRM);
+	}
+	
+	public void testCadastrarProfissionalComFalhas(){
+		Exception exception = new Exception();	
+		//Tipo null
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude(null, "123null456", "Dr Fake", "Placebologista", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
 		assertNotNull(exception);
-		exception = null;
+		
+		exception = new Exception();	
+		//CRM null
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", null, "Dr Fake", "Placebologista", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+		
+		exception = new Exception();	
+		//CRM string vazia
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", "", "Dr Fake", "Placebologista", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+		
+		exception = new Exception();	
+		//Nome null
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", "314156", null, "Placebologista", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+		
+		exception = new Exception();	
+		//Nome vazio
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", "314156", "", "Placebologista", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+		
+		exception = new Exception();	
+		//Especialidade null
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", "314156", "Dr Fake", null, "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+		
+		exception = new Exception();	
+		//Especialidade vazio
+		try {
+			ProfissionalSaude p5 = new ProfissionalSaude("Médico", "314156", "Dr Fake", "", "UniT");
+			crmProfsFake.add(p5.getNumeroRegistro());
+			exception = null;
+			
+		} catch (ProfissionalSaudeException e) {
+			e.printStackTrace();
+			exception = e;
+		}
+		assertNotNull(exception);
+	}
+	
+	private void removeProfissinaisFakeFromTest() {
+		System.out.println("CRM " + crmProfsFake.size());
 		
 		try {
-			ProfissionalSaude p4 = new ProfissionalSaude("Medico", "1010101", "Timothy Leary", "Experimentalista", "Particular");
-			DAOParse.getInstance().cadastrarProfissional(p4);
-			
-		} catch (ProfissionalSaudeException e) {
-			e.printStackTrace();
-			
-		}catch (ParseException e) {
-			exception = e;
+			DAOParse.getInstance().removerProfissional("abc-124");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		assertNotNull(exception);
-		
+		try {
+			DAOParse.getInstance().removerProfissional("1010101");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			DAOParse.getInstance().removerProfissional("101010100001111000011110000111100001111");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			DAOParse.getInstance().removerProfissional("101010100001111000011110000111100001110");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
